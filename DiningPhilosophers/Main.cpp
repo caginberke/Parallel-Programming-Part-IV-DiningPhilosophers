0f2930dc96df008b2d1dffc483b5eb74d4eb7f47
caginberke <caginberke416@gmail.com>
2024-11-23 15:42:52 +0300

    .

--- DiningPhilosophers/Main.cpp
+++ DiningPhilosophers/Main.cpp
@@ -23,12 +23,10 @@ enum StickState {
     InUse
 };
 
-void EatingAnimation(){
-//TODO - Filling red box
+void EatingAnimation() {
 }
 
-void DrawTable() {
-    // Draw the table (circle)
+void DrawTable(int philosopherStates[SP_NUMBER], int stickStates[SP_NUMBER]) {
     FillCircle(screen, TABLE_CENTER_X, TABLE_CENTER_Y, TABLE_RADIUS, 0xff4e3b);
     float angleIncrement = 2 * PI / SP_NUMBER;
 
@@ -36,8 +34,13 @@ void DrawTable() {
         float angle = i * angleIncrement;
         int currX = TABLE_CENTER_X + (int)(TABLE_RADIUS * cos(angle));
         int currY = TABLE_CENTER_Y + (int)(TABLE_RADIUS * sin(angle));
-        
-        FillCircle(screen, currX, currY, PHILOSOPHER_RADIUS, 0x00ff00); 
+
+        int color; 
+        if (philosopherStates[i] == Thinking) color = 0x00ff00; //Green - Thinking
+        else if (philosopherStates[i] == Eating) color = 0xeb0909; //Red - Eating
+        else if (philosopherStates[i] == Hungry) color = 0xffff00; //Yellow - Hungry
+
+        FillCircle(screen, currX, currY, PHILOSOPHER_RADIUS, color);
 
         angle = ((i + 1) % SP_NUMBER) * angleIncrement;
         int nextX = TABLE_CENTER_X + (int)(TABLE_RADIUS * cos(angle));
@@ -46,11 +49,11 @@ void DrawTable() {
         int stickCenterX = (currX + nextX) / 2;
         int stickCenterY = (currY + nextY) / 2;
 
-        FillRect(screen, stickCenterX - STICK_LENGTH / 2, stickCenterY - STICK_HEIGHT / 2, STICK_LENGTH, STICK_HEIGHT, 0xff0000);
+        int stickColor = stickStates[i] == InUse ? 0xff0000 : 0x40ff00; //Red InUse, Green Available
+        FillRect(screen, stickCenterX - STICK_LENGTH / 2, stickCenterY - STICK_HEIGHT / 2, STICK_LENGTH, STICK_HEIGHT, stickColor);
     }
     DisplayImage(FRM, screen);
 }
-
 
 class Sticks {
 public:
@@ -70,6 +73,7 @@ public:
     void eat();
     void pickSticks();
     void putSticks();
+    PhilosopherState getState() { return state; } // Accessor for philosopher state
 
 private:
     int id;
@@ -77,47 +81,64 @@ private:
     PhilosopherState state;
 };
 
-
-
 Sticks::Sticks() {
     for (int i = 0; i < SP_NUMBER; i++) {
         stickStates[i] = Available;
     }
 }
-void Sticks::take(int stickId) {stickStates[stickId] = InUse;}
-void Sticks::release(int stickId) {stickStates[stickId] = Available;}
-bool Sticks::isAvailable(int stickId) {return stickStates[stickId] == Available;}
+void Sticks::take(int stickId) { stickStates[stickId] = InUse; }
+void Sticks::release(int stickId) { stickStates[stickId] = Available; }
+bool Sticks::isAvailable(int stickId) { return stickStates[stickId] == Available; }
 
 Philosophers::Philosophers(int id, Sticks& sticks) : id(id), sticks(sticks), state(Thinking) {}
-void Philosophers::think() {state = Thinking;}
+void Philosophers::think() { state = Thinking; }
 void Philosophers::eat() {
     state = Eating;
     pickSticks();
     EatingAnimation();
-    Sleep(200);    
-    putSticks();   
+    Sleep(0);
+    putSticks();
 }
-void Philosophers::pickSticks() { if (sticks.isAvailable(id) && sticks.isAvailable((id + 1) % SP_NUMBER)) { sticks.take(id); sticks.take((id + 1) % SP_NUMBER); state = Eating; } else state = Hungry; }
-void Philosophers::putSticks() { sticks.release(id); sticks.release((id + 1) % SP_NUMBER); }
+void Philosophers::pickSticks() {
+    if (sticks.isAvailable(id) && sticks.isAvailable((id + 1) % SP_NUMBER)) {
+        sticks.take(id);
+        sticks.take((id + 1) % SP_NUMBER);
+        state = Eating;
+    }
+    else {
+        state = Hungry;
+    }
+}
+void Philosophers::putSticks() {
+    sticks.release(id);
+    sticks.release((id + 1) % SP_NUMBER);
+}
 
-void ICGUI_Create() {ICG_MWTitle("Dining Philosophers"); ICG_MWSize(790, 610);}
-void Table(){CreateImage(screen, 560, 620, ICB_UINT);}
+void ICGUI_Create() { ICG_MWTitle("Dining Philosophers"); ICG_MWSize(790, 610); }
 
-void Start(void* simFunction) {CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)simFunction, NULL, 0, NULL); CreateImage(screen, 740, 520, ICB_UINT);}
+void Table() {
+    CreateImage(screen, 560, 620, ICB_UINT);
+}
 
+void Start(void* simFunction) {
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)simFunction, NULL, 0, NULL);
+    CreateImage(screen, 740, 520, ICB_UINT);
+}
 
 void Deadlock() {
-    DrawTable();
+    int philosopherStates[SP_NUMBER] = { Eating, Available, Hungry, Eating, Thinking };
+    int stickStates[SP_NUMBER] = { InUse, Available, InUse, Available, InUse };
+    DrawTable(philosopherStates, stickStates);
 }
-void Semaphore(){
-    DrawTable();
+
+void Semaphore() {
+    int philosopherStates[SP_NUMBER] = { Thinking, Hungry, Eating, Thinking, Eating };
+    int stickStates[SP_NUMBER] = { Available, Available, InUse, Available, Available };
+    DrawTable(philosopherStates, stickStates);
 }
 
 void ICGUI_main() {
     ICG_Button(10, 5, 120, 25, "Deadlock", Start, (void*)Deadlock);
     ICG_Button(135, 5, 120, 25, "Semaphore", Start, (void*)Semaphore);
     FRM = ICG_FrameMedium(10, 30, 750, 520);
-    
 }
-
-
